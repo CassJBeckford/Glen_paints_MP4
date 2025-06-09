41a71b34847a86e35c7c9cfdcaddcf14ad84c9cd
cassjbeckford <cassjbeckford282@outlook.com>
2025-06-09 18:09:45 +0100

    testing server error

--- faqs_2/admin.py
+++ faqs_2/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import FAQ_2
 
-# Register your models here.
+
+@admin.register(FAQ_2)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'category')
+    search_fields = ('question', 'category', 'answer')
--- faqs_2/models.py
+++ faqs_2/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class FAQ(models.Model):
+class FAQ_2(models.Model):
     question = models.CharField(max_length=255)
     answer = models.TextField()
     category = models.CharField(max_length=50, blank=True, null=True)
